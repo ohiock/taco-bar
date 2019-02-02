d1f27b10f2f401437ac2702c931fb4a43911f6bd
Terrence Bowman <tbowman.oh@gmail.com>
2019-02-02 11:55:03 -0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  margin: 0;
+  padding: 0;
+
+  font-family: sans-serif;
+`;
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <AppContainer>This is the app</AppContainer>;
   }
 }
 
